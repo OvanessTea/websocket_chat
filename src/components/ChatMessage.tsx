86b2214486caa7d2bf329754f3f404725a27c7bb
OvanessTea <OvanessTea@yandex.com>
2025-02-14 06:39:02 +0100

    fix styles of system msgs

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -6,15 +6,23 @@ interface ChatMessageProps {
     isOwnMessage: boolean;
 }
 
-const ChatMessage: React.FC<ChatMessageProps> = ({ message, isOwnMessage }) => (
-    <div className={`mb-2 ${isOwnMessage ? 'text-right' : 'text-left'}`}>
-        <span className={`inline-block p-2 rounded-lg ${isOwnMessage ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}>
-            <strong>{message.user}: </strong>{message.text}
-        </span>
-        <div className="text-xs text-gray-500 mt-1">
-            {new Date(message.timestamp).toLocaleTimeString()}
-        </div>
-    </div>
-)
+const ChatMessage: React.FC<ChatMessageProps> = ({ message, isOwnMessage }) => {
+
+    return (
+        message.system ? (
+            <div className="mb-2 text-center text-gray-500">
+                {message.text}
+            </div>
+        ) : (
+            <div className={`mb-2 ${isOwnMessage ? 'text-right' : 'text-left'}`}>
+                <span className={`inline-block p-2 rounded-lg ${isOwnMessage ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}>
+                    <strong>{message.user}: </strong>{message.text}
+                </span>
+                <div className="text-xs text-gray-500 mt-1">
+                    {new Date(message.timestamp).toLocaleTimeString()}
+                </div>
+            </div>)
+    )
+}
 
 export default ChatMessage;
